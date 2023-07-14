433c2875317099932aa6f4bc31ed42398f1db2f1
Th1sD0t <robin.wulz@outlook.com>
2023-07-14 13:44:14 +0200

    SEG Branding

--- Toolkit/Deploy-Application.ps1
+++ Toolkit/Deploy-Application.ps1
@@ -1,10 +1,8 @@
 ﻿<#
 .SYNOPSIS
-
 PSApppDeployToolkit - This script performs the installation or uninstallation of an application(s).
 
 .DESCRIPTION
-
 - The script is provided as a template to perform an install or uninstall of an application(s).
 - The script either performs an "Install" deployment type or an "Uninstall" deployment type.
 - The install deployment type is broken down into 3 main sections/phases: Pre-Install, Install, and Post-Install.
@@ -19,62 +17,49 @@ WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FI
 for more details. You should have received a copy of the GNU Lesser General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 .PARAMETER DeploymentType
-
 The type of deployment to perform. Default is: Install.
 
 .PARAMETER DeployMode
-
 Specifies whether the installation should be run in Interactive, Silent, or NonInteractive mode. Default is: Interactive. Options: Interactive = Shows dialogs, Silent = No dialogs, NonInteractive = Very silent, i.e. no blocking apps. NonInteractive mode is automatically set if it is detected that the process is not user interactive.
 
 .PARAMETER AllowRebootPassThru
-
 Allows the 3010 return code (requires restart) to be passed back to the parent process (e.g. SCCM) if detected from an installation. If 3010 is passed back to SCCM, a reboot prompt will be triggered.
 
 .PARAMETER TerminalServerMode
-
 Changes to "user install mode" and back to "user execute mode" for installing/uninstalling applications for Remote Desktop Session Hosts/Citrix servers.
 
 .PARAMETER DisableLogging
-
 Disables logging to file for the script. Default is: $false.
 
 .EXAMPLE
-
 powershell.exe -Command "& { & '.\Deploy-Application.ps1' -DeployMode 'Silent'; Exit $LastExitCode }"
 
 .EXAMPLE
-
 powershell.exe -Command "& { & '.\Deploy-Application.ps1' -AllowRebootPassThru; Exit $LastExitCode }"
 
 .EXAMPLE
-
 powershell.exe -Command "& { & '.\Deploy-Application.ps1' -DeploymentType 'Uninstall'; Exit $LastExitCode }"
 
 .EXAMPLE
-
 Deploy-Application.exe -DeploymentType "Install" -DeployMode "Silent"
 
 .INPUTS
-
 None
 
 You cannot pipe objects to this script.
 
 .OUTPUTS
-
 None
 
 This script does not generate any output.
 
 .NOTES
-
 Toolkit Exit Code Ranges:
 - 60000 - 68999: Reserved for built-in exit codes in Deploy-Application.ps1, Deploy-Application.exe, and AppDeployToolkitMain.ps1
 - 69000 - 69999: Recommended for user customized exit codes in Deploy-Application.ps1
 - 70000 - 79999: Recommended for user customized exit codes in AppDeployToolkitExtensions.ps1
 
 .LINK
-
 https://psappdeploytoolkit.com
 #>
 
@@ -181,9 +166,6 @@ Try {
         ##*===============================================
         [String]$installPhase = 'Pre-Installation'
 
-        ## Show Welcome Message, close Internet Explorer if required, allow up to 3 deferrals, verify there is enough disk space to complete the install, and persist the prompt
-        # Show-InstallationWelcome -CloseApps 'iexplore' -AllowDefer -DeferTimes 3 -CheckDiskSpace -PersistPrompt
-
         ## Show Progress Message (with the default message)
         Show-InstallationProgress
 
@@ -215,19 +197,12 @@ Try {
 
         ## <Perform Post-Installation tasks here>
 
-        ## Display a message at the end of the install
-        # If (-not $useDefaultMsi) {
-        #     Show-InstallationPrompt -Message 'You can customize text to appear at the end of an install or remove it completely for unattended installations.' -ButtonRightText 'OK' -Icon Information -NoWait
-        # }
     }
     ElseIf ($deploymentType -ieq 'Uninstall') {
         ##*===============================================
         ##* PRE-UNINSTALLATION
         ##*===============================================
         [String]$installPhase = 'Pre-Uninstallation'
-
-        ## Show Welcome Message, close Internet Explorer with a 60 second countdown before automatically closing
-        # Show-InstallationWelcome -CloseApps 'iexplore' -CloseAppsCountdown 60
 
         ## Show Progress Message (with the default message)
         Show-InstallationProgress
@@ -265,9 +240,6 @@ Try {
         ##* PRE-REPAIR
         ##*===============================================
         [String]$installPhase = 'Pre-Repair'
-
-        ## Show Welcome Message, close Internet Explorer with a 60 second countdown before automatically closing
-        # Show-InstallationWelcome -CloseApps 'iexplore' -CloseAppsCountdown 60
 
         ## Show Progress Message (with the default message)
         Show-InstallationProgress
